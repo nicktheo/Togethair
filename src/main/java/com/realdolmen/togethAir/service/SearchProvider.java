a6bea98d3c2c3b6b5d8bc3a6e50c574d5792e84a
Nikos Theodorakis <nikolaos.theodorakis@realdolmen.com>
2017-11-08 11:30:57 +0100

    small fixes

--- src/main/java/com/realdolmen/togethAir/service/SearchProvider.java
+++ src/main/java/com/realdolmen/togethAir/service/SearchProvider.java
@@ -1,7 +1,7 @@
 package com.realdolmen.togethair.service;
 
-import com.realdolmen.togethair.domain.PlaneClassType;
-import com.realdolmen.togethair.domain.Seat;
+import com.realdolmen.togethair.domain.flight.TravelClassType;
+import com.realdolmen.togethair.domain.flight.Seat;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,13 +9,13 @@ import java.util.List;
 
 public class SearchProvider {
 
-    public List<Seat> searchFlight(int numberOfSeats, PlaneClassType travellingClass, Object departure, Object destination, Date date) {
+    public List<Seat> searchFlight(int numberOfSeats, TravelClassType travellingClass, Object departure, Object destination, Date date) {
 
         //sort out airports or global regions as parameters (generics?)
 
         List<Seat> availableSeats = new ArrayList<>(); //list of available seats to return
 
-
+        return null;
 
     }
 
--- src/test/java/com/realdolmen/Togethair/domain/FlightTest.java
+++ src/test/java/com/realdolmen/Togethair/domain/FlightTest.java
@@ -1,13 +1,21 @@
-package com.realdolmen.togethAir.domain;
+package com.realdolmen.togethair.domain;
 
-import com.realdolmen.togethAir.domain.*;
+import com.realdolmen.togethair.domain.location.Airport;
+import com.realdolmen.togethair.domain.location.GlobalRegion;
+import com.realdolmen.togethair.domain.flight.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class FlightTest {
 
-    Flight flight;
+    Flight Flight;
 
     @Before
     public void initialize(){
@@ -15,15 +23,31 @@ public class FlightTest {
         Airport departureAirport = new Airport("Brussels Airport", "Belgium","BRU", GlobalRegion.NORTHERN_EUROPE);
         Airport destinationAirport = new Airport("Athens International Airport", "Greece","ATH", GlobalRegion.SOUTHERN_EUROPE);
 
-        flight = new Flight(departureAirport,destinationAirport);
+        DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(sdf.format(date));
+        TravelClass planeClassFirst;
+        Seat seat = new Seat(5, 10, Availability.FREE, new TravelClass());
+        List<Seat> seats = new ArrayList<>();
+        List<TravelClass> availability = new ArrayList<>();
+
+        seats.add(seat);
+
+        planeClassFirst = new TravelClass(TravelClassType.FIRST,200,seats,Flight);
+        availability.add(planeClassFirst);
+        Flight flight = new Flight(departureAirport,destinationAirport,date,"3 hours",availability);
     }
 
     @Test
-    public void testFlight() {
-        Assert.assertEquals(GlobalRegion.NORTHERN_EUROPE,flight.getDepartureAirport().getGlobalRegion());
-        Assert.assertEquals(GlobalRegion.SOUTHERN_EUROPE,flight.getDestinationAirport().getGlobalRegion());
-        Assert.assertEquals("Brussels Airport",flight.getDepartureAirport().getName());
-        Assert.assertEquals("Athens International Airport",flight.getDestinationAirport().getName());
-    }
+    public void testSpecificFlight() {
+        Assert.assertEquals(GlobalRegion.NORTHERN_EUROPE,Flight.getDepartureAirport().getGlobalRegion());
+        Assert.assertEquals(GlobalRegion.SOUTHERN_EUROPE,Flight.getDestinationAirport().getGlobalRegion());
+        Assert.assertEquals("Brussels Airport",Flight.getDepartureAirport().getName());
+        Assert.assertEquals("Athens International Airport",Flight.getDestinationAirport().getName());
 
+        Assert.assertEquals(2017,Flight.getDateTime().getYear()+1900); // wtf
+
+        Assert.assertEquals(TravelClassType.FIRST,Flight.getAvailability().get(0).getTravelClassType());
+
+    }
 }

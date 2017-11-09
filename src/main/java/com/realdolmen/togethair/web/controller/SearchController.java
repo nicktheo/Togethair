b202b9af66e962f9b2e76fab345cfdeb83445193
Karsten Goossens <karsten.goossens@realdolmen.com>
2017-11-09 13:58:44 +0100

    add the searchController to git to be able to pull nick's work

---
+++ src/main/java/com/realdolmen/togethair/web/controller/SearchController.java
@@ -0,0 +1,11 @@
+package com.realdolmen.togethair.web.controller;
+
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+@Named
+@FlowScoped("search")
+public class SearchController {
+
+    
+}
